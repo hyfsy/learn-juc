152ad6529df37a058ebe555acb4047ec23b95a44
hyfsy <1577975140@qq.com>
2022-02-13 13:38:46 +0800

    合并写

---
+++ src/test/java/com/hyf/juc/test/Test_WriteCombining.java
@@ -0,0 +1,73 @@
+package com.hyf.juc.test;
+
+/**
+ * CPU 合并写
+ * <p>
+ * L1缓存和ALU之间还有一个 WC Buffer，里面只有四个字节，满了后，一次性写到内存中
+ * <p>
+ * 失败？
+ *
+ * @author baB_hyf
+ * @date 2022/02/13
+ */
+public class Test_WriteCombining {
+
+    private static final int ITERATIONS = Integer.MAX_VALUE;
+    private static final int ITEMS      = 1 << 24;
+    private static final int MASK       = ITEMS - 1;
+
+    private static final byte[] arrayA = new byte[ITEMS];
+    private static final byte[] arrayB = new byte[ITEMS];
+    private static final byte[] arrayC = new byte[ITEMS];
+    private static final byte[] arrayD = new byte[ITEMS];
+    private static final byte[] arrayE = new byte[ITEMS];
+    private static final byte[] arrayF = new byte[ITEMS];
+
+    public static void main(final String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i + " SingleLoop duration (ns) = " + runCaseOne());
+            System.out.println(i + " SplitLoop  duration (ns) = " + runCaseTwo());
+        }
+        int result = arrayA[1] + arrayB[2] + arrayC[3] + arrayD[4] + arrayE[5] + arrayF[6];
+        System.out.println("result = " + result);
+    }
+
+    public static long runCaseOne() {
+        long start = System.nanoTime();
+        int i = ITERATIONS;
+
+        while (--i != 0) {
+            int slot = i & MASK;
+            byte b = (byte) i;
+            arrayA[slot] = b;
+            arrayB[slot] = b;
+            arrayC[slot] = b;
+            arrayD[slot] = b;
+            arrayE[slot] = b;
+            arrayF[slot] = b;
+        }
+        return System.nanoTime() - start;
+    }
+
+    public static long runCaseTwo() {
+        long start = System.nanoTime();
+        int i = ITERATIONS;
+        while (--i != 0) {
+            int slot = i & MASK;
+            byte b = (byte) i; // 第四个
+            arrayA[slot] = b;
+            arrayB[slot] = b;
+            arrayC[slot] = b;
+        }
+        i = ITERATIONS;
+        while (--i != 0) {
+            int slot = i & MASK;
+            byte b = (byte) i;
+            arrayD[slot] = b;
+            arrayE[slot] = b;
+            arrayF[slot] = b;
+        }
+        return System.nanoTime() - start;
+    }
+
+}
