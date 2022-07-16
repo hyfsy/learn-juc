644b8ab93903345bc6ea73ac88bf43c65b7f539f
hyfsy <1577975140@qq.com>
2022-07-16 12:57:35 +0800

    readWriteLock持有锁顺序导致的阻塞测试

---
+++ src/test/java/com/hyf/juc/lock/Lock_ReadWrite_Notice.java
@@ -0,0 +1,30 @@
+package com.hyf.juc.lock;
+
+import org.junit.Test;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 读写锁测试
+ *
+ * @author baB_hyf
+ * @date 2020/12/06
+ */
+public class Lock_ReadWrite_Notice {
+
+    private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    @Test
+    public void notBlock() {
+        readWriteLock.writeLock().lock();
+        readWriteLock.readLock().lock();
+        System.out.println(1);
+    }
+
+    @Test
+    public void block() {
+        readWriteLock.readLock().lock();
+        readWriteLock.writeLock().lock(); // block
+        System.out.println(1);
+    }
+}
